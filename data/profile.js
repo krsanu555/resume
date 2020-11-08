d5c272e952e40afa8ea53129ef76636a8d725fab
Kumar Sanu <46781348+krsanu555@users.noreply.github.com>
2020-11-08 12:52:49 +0530

    Update profile.js

--- data/profile.js
+++ data/profile.js
@@ -43,22 +43,22 @@ const profileData = {
     {
       graph: 'false',
       category: 'Programming Languages',
-      topics: ['Python','C#','JavaScript']
+      topics: ['C#','Python','Jquery/JavaScript','HTML','CSS']
     },
     {
       graph: 'false',
       category: 'Databases',
-      topics: ['MySQL', 'SQL']
+      topics: ['MS SQL','MySQL',]
     },
     {
       graph: 'false',
       category: 'Frameworks / Libraries',
-      topics: ['.Net Standard','.Net Core', 'Angular','Django']
+      topics: ['ASP.NET MVC','ASP.NET','.Net Standard','.Net Core', 'Angular 8','Django']
     },
    {
       graph: 'false',
       category: 'DEV Tools',
-      topics: ['Visual Studio', 'VS Code','Pycharm','Git','Postman']
+      topics: ['Visual Studio 17/19', 'VS Code','Pycharm','Git','Postman']
     }
   ],
   projects: [
@@ -78,21 +78,21 @@ const profileData = {
       desc: `Sr Software Engineer`,
       date: 'Nov 2018 - Present',
       details: [
-        `Contributed in the development of an application where we have used <strong>Angular</strong> in the front-end,
-         <strong>Python</strong> with <strong>Django Rest Framework</strong> in the backend and <strong>MySQL</strong> for database.`,
+        `Worked on Authentication & Authorization of <strong>ASP.NET Web APIs</strong> using AD Groups and made it accessible only through Client Network.`,
 	`Created RESTful <strong> ASP.NET Web APIs </strong> from scratch and incorporated Data Access layers removed from Web Application in it.Hence we have converted that
-         web application into a <strong>client server</strong> application.`,
-	`Worked on Authentication & Authorization of <strong>ASP.NET Web APIs</strong> using AD Groups and made it accessible only through Client Network.`,
+         web application into a <strong>client server</strong> application.`,	
         `<strong>Created</strong> and <strong>Secured</strong> build & config profiles for different <strong>environments</strong> i.e. Development,QA and 
          Production for many .Net applications.`,
 	`Added new functionalities & modified existing functionalities in many <strong>.Net</strong> Applications as a part of enhancement tasks as per the client's expectations.`,
-	`Worked on <strong>report</strong> generation task from database in Excel Format.`,
-	`<strong>Optimized</strong> one method for generating report in Excel format through database restructuring and some code changes for the associated application by <strong>40%</strong>.`,
-	`Made one .Net <strong>mobile</strong> responsive application more <strong>user friendly</strong>.`,
-	`Resolved more than <strong>50 bugs/issues</strong> in existing .Net applications of client.`,
 	`Worked on multiple applications of same client that uses <strong>C#</strong> as programming language,<strong>ASP.NET,ASP.NET MVC, ASP.NET WEB API</strong> as web 
          technology and <strong>MS SQL Server</strong> as database.`,
 	`Worked on both <strong>ADO.Net</strong> And <strong>Entity Framework</strong> approach of .Net Framework.`,
+	`Resolved more than <strong>50 bugs/issues</strong> in existing .Net applications of client.`,
+	`Worked on <strong>report</strong> generation task from database in Excel Format.`,
+	`<strong>Optimized</strong> one method for generating report in Excel format through database restructuring and some code changes for the associated application by <strong>40%</strong>.`,
+	`Worked on one application to make more user friendly through use of CSS & Bootstrap.`,	
+	`Contributed in the development of an application where we have used <strong>Angular</strong> in the front-end,
+         <strong>Python</strong> with <strong>Django Rest Framework</strong> in the backend and <strong>MySQL</strong> for database.`,
 	`Worked in <strong>Agile environment</strong> and used <strong>Azure Devops</strong> CI/CD pipeline for deploying software.`,
 	`Knowledge of <strong>NUnit</strong> unit-testing framework and <strong>.Net Core</strong> Web API.`
       ]
