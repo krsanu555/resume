b3994c2de7861a67a50ab84349c61b0ef8e54b36
Kumar Sanu <46781348+krsanu555@users.noreply.github.com>
2020-11-10 12:16:02 +0530

    Update profile.js

--- data/profile.js
+++ data/profile.js
@@ -77,19 +77,17 @@ const profileData = {
       title: `Sr Software Engineer`,
       desc: `Sr Software Engineer`,
       date: 'Nov 2018 - Present',
-      details: [
-        `Worked on Authentication & Authorization of <strong>ASP.NET Web APIs</strong> using AD Groups and made it accessible only through Client Network.`,
-	`Created RESTful <strong> ASP.NET Web APIs </strong> from scratch and incorporated Data Access layers removed from Web Application in it.Hence we have converted that
-         web application into a <strong>client server</strong> application.`,	
+      details: [     
+	`Converted a simple web application into a client server application by creating RESTful ASP.NET Web APIs from scratch and incorporated Data Access layers 
+         removed from Web Application in it. Also Worked on Authentication & Authorization of <strong>ASP.NET Web APIs</strong> using AD Groups and made it accessible 
+         only through Client Network.`,	
         `Created and Secured <strong>build & config</strong> profiles for different <strong>server</strong> environments i.e. Development,QA and 
-         Production for many .Net applications.`,
-	`Added new functionalities & modified existing functionalities in many <strong>.Net</strong> Applications as a part of enhancement tasks as per the client's expectations.`,
-	`Worked on multiple applications of same client that uses <strong>C#</strong> as programming language,<strong>ASP.NET,ASP.NET MVC, ASP.NET WEB API</strong> as web 
+         Production for many .Net applications.`,	      
+	`Worked on enhancement tasks as per the client's expectations in multiple applications that uses <strong>C#</strong> as programming language,<strong>ASP.NET,ASP.NET MVC, ASP.NET WEB API</strong> as web 
          technology and <strong>MS SQL Server</strong> as database.`,
 	`Worked on both <strong>ADO.Net</strong> And <strong>Entity Framework</strong> approach of .Net Framework.`,
-	`Resolved more than <strong>50 bugs/issues</strong> in existing .Net applications of client.`,
-	`Worked on <strong>report</strong> generation task from database in Excel Format.`,
-	`<strong>Optimized</strong> one method for generating report in Excel format through database restructuring and some code changes for the associated application by <strong>40%</strong>.`,
+	`Resolved more than <strong>50 bugs/issues</strong> in existing .Net applications of client.`,	
+	`Worked on <strong>report</strong> generation task and <strong>Optimized</strong> one method for generating report in Excel format through database restructuring and some code changes for the associated application by <strong>40%</strong>.`,
 	`Worked on one application to make more <strong>user friendly</strong> through use of CSS & Bootstrap.`,	
 	`Contributed in the development of an application where we have used <strong>Angular</strong> in the front-end,
          <strong>Python</strong> with <strong>Django Rest Framework</strong> in the backend and <strong>MySQL</strong> for database.`,
