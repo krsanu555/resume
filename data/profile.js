8193f2862f67471637e59997f3c2811de7b18db8
Kumar Sanu <46781348+krsanu555@users.noreply.github.com>
2022-01-10 16:15:04 +0530

    Update profile.js

--- data/profile.js
+++ data/profile.js
@@ -97,10 +97,10 @@ const profileData = {
       desc: `Software Engineer`,
       date: 'Apr 2021 - Present',
       details: [
-        `Developed and incorporated new features in HealthCare Product.`,
-        `Optimised product performance by reducing overall turn around time of case processing.`,
-	`Worked on ML algorithm integration in the app.`,
-	`Improved DICOM Structured Report and Result sent to PACS.`
+        `Developed and incorporated <strong>new features</strong> in HealthCare Product.`,
+        `<strong>Optimised</strong> product performance by reducing overall turn around time of <strong>case processing</strong>.`,
+	`Worked on <strong>ML algorithm</strong> integration in the app.`,
+	`Improved <strong>DICOM Structured Report</strong> and <strong>Result</strong> sent to PACS.`
        ]
     },  
     {
@@ -114,9 +114,8 @@ const profileData = {
 	 /*`Created and Secured <strong>build & config</strong> profiles for different <strong>deployment</strong> environments i.e. Development,QA and
          Production for many .Net applications.`, */    
         `<strong>Optimized</strong> one method for generating report in Excel format through database restructuring and some code changes for the associated application by <strong>40%</strong>.`,	
-	`Worked on enhancement tasks as per the client's expectations in multiple applications that uses <strong>C#</strong> as programming language,<strong>ASP.NET,ASP.NET MVC, ASP.NET WEB API</strong> as web 
-         technology,<strong>ADO.Net</strong>,<strong>Entity Framework</strong> for data access operations and <strong>MS SQL Server</strong> as database. Resolved more than 
-         <strong>50 bugs/issues</strong> in existing applications.`,
+	`Worked on enhancement tasks in multiple applications that uses <strong>C#</strong> as programming language,<strong>ASP.NET,ASP.NET MVC, ASP.NET WEB API</strong> as web 
+         technology,<strong>ADO.Net</strong>,<strong>Entity Framework</strong> for data access operations and <strong>MS SQL Server</strong> as database.`,
 	`Contributed in the development of an application where we have used <strong>Angular</strong> in the front-end,
          <strong>Python</strong> with <strong>Django Rest Framework</strong> in the backend and <strong>MySQL</strong> for database.`,
 	`Worked in <strong>Agile environment</strong> and used <strong>Azure Devops</strong> CI/CD pipeline for deploying software.`
